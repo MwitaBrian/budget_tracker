26c53f540dcf2cfdc94519128f7800eb97ccea15
MwitaBrian <brianmwita13@gmail.com>
2023-03-03 05:26:00 +0300

    Add header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import { Button, Container, Stack } from "react-bootstrap";
 
 function App() {
   return(
     <>
-      <h1>Budget Tracker</h1>
+      <Container className="my-3">
+        <h1>Budget Tracker</h1>
+        <Stack direction="horizontal" gap="3" className="mb-4">
+          <h3 className="me-auto">Budgets</h3>
+          <Button variant="primary">Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+      </Container>
     </>
   )
 }
